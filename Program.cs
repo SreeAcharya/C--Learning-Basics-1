66c9f406d5aeac5b7e35dc1b7105d5d5aef6a332
SreeAcharya <111042936+SreeAcharya@users.noreply.github.com>
2026-06-06 13:09:10 +0530

    Add files via upload
    REcangle Area and PErineter?

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _02_RectangleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Length: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int area = length * width;
+            Console.WriteLine("\nArea: " + area);
+            int perimeter = 2 * (length*width);
+            Console.WriteLine("Perimter:" + perimeter);
+        }
+    }
+}
