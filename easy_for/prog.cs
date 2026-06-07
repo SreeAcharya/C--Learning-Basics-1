b7e69645d67f3e616c178f0506febd04b5bab03f
SreeAcharya <111042936+SreeAcharya@users.noreply.github.com>
2026-06-07 15:25:08 +0530

    Create prog.cs

---
+++ easy_for/prog.cs
@@ -0,0 +1,13 @@
+namespace _1_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 20; i>=0; i-=2)
+            {
+                Console.WriteLine(i + "");
+            }
+        }
+    }
+}
