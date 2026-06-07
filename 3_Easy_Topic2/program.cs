9b85ab7744c4d121dcef34634b2fc2688d949678
SreeAcharya <111042936+SreeAcharya@users.noreply.github.com>
2026-06-07 11:06:42 +0530

    Create program.cs

---
+++ 3_Easy_Topic2/program.cs
@@ -0,0 +1,14 @@
+namespace _03_Easy_Topic2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i >=1; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Blast off!");
+        }
+    }
+}
